201902e6258d4c54e8b2d6dd1fd0aa8a8a32f285
Ab-Wb-Sid <aw1411175@gmail.com>
2026-07-14 10:18:29 +0500

    deleted migration

--- next-sanestix/src/components/ui/GradientText.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from 'react';
-
-interface GradientTextProps {
-  children: ReactNode;
-}
-
-export default function GradientText({ children }: GradientTextProps) {
-  return <span className="grad">{children}</span>;
-}
